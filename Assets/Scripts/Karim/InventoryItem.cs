8ef771a0cd1b74e60f5cad18eb5e578e3a85e638
crimson-king-5 <65815180+crimson-king-5@users.noreply.github.com>
2021-05-18 13:07:02 +0200

    FEAT (Iventory) : iventory manager.

---
+++ Assets/Scripts/Karim/InventoryItem.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Items")]
+public class InventoryItem : ScriptableObject
+{
+    public string itemName;
+    public string itemDescription;
+    public Sprite itemImage;
+    public int numberHeld;
+    
+}
