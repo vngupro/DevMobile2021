931cc9a074593f3941bdd1f89f7fe63df915f993
nguyen <virginie.nguyen@edu.devinci.fr>
2021-05-19 17:15:31 +0200

    fix (controller) : pick up layer

--- Assets/Scripts/Karim/Item.cs
+++ Assets/Scripts/Karim/Item.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     public InventoryItem data;
-
+    private SpriteRenderer spriteRenderer;
     private void Awake()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         if (data == null)
         {
             Debug.Log("Item : " + this.name + " has no data !\nPlease add an Inventory Item (Scriptable Object) in \"Data\"");
+        }
+        else
+        {
+            if(data.itemImage == null)
+            {
+                Debug.Log("Item : " + this.name + " has no Sprite in scriptable object !");
+            }
+            else
+            {
+                spriteRenderer.sprite = data.itemImage;
+            }
         }
     }
 }
