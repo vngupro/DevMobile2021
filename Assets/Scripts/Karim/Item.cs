f0e614c2e87118e35788bdb86a1110e7a029b2db
nguyen <virginie.nguyen@edu.devinci.fr>
2021-05-19 16:36:38 +0200

    add item prefab

--- Assets/Scripts/Karim/Item.cs
+++ Assets/Scripts/Karim/Item.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     public InventoryItem data;
+
+    private void Awake()
+    {
+        if (data == null)
+        {
+            Debug.Log("Item : " + this.name + " has no data !\nPlease add an Inventory Item (Scriptable Object) in \"Data\"");
+        }
+    }
 }
