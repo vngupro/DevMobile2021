ba91d2d51f29a8a5ed910d9750efa3d5b78a1480
EstebanNunez <esteban78570@gmail.com>
2021-05-25 17:29:15 +0200

    Implement PlayService

---
+++ Assets/Scripts/Esteban/TestPlayService.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+
+public class TestPlayService : MonoBehaviour
+{
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+
+        try
+        {
+            PlayGamesClientConfiguration configuration = new PlayGamesClientConfiguration.Builder().Build();
+            PlayGamesPlatform.InitializeInstance(configuration);
+            PlayGamesPlatform.DebugLogEnabled = true;
+            PlayGamesPlatform.Activate();
+            Social.localUser.Authenticate((bool success) => { });
+        }
+        catch (Exception exception)
+        {
+            Debug.Log(exception);
+        }
+
+    }
+
+    public void ShowAchivments()
+    {
+        // show achievements UI
+        Social.ShowAchievementsUI();
+    }
+    
+    public void ShowLeaderboard()
+    {
+        // show leaderboard UI
+        Social.ShowLeaderboardUI();
+    }
+
+}
