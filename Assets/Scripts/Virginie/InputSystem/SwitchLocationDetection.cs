48f8b159e1c5f706c42be684590a1b7f0958c039
nguyen <virginie.nguyen@edu.devinci.fr>
2021-05-28 03:10:12 +0200

    fix (door) : less sensibility

--- Assets/Scripts/Virginie/InputSystem/SwitchLocationDetection.cs
+++ Assets/Scripts/Virginie/InputSystem/SwitchLocationDetection.cs
@@ -6,11 +6,16 @@ public class SwitchLocationDetection : MonoBehaviour
 {
     #region Variable
     [SerializeField] private LayerMask layer;
+    [SerializeField] private float distanceTolerance = 0.5f;         //sensibility on small sliding on touch
+    [SerializeField] private float timerBeforeHold = 1.0f;
+
+    private Vector2 startPos;
+    private Vector2 endPos;
+    private float startTime;
+    private float endTime;
 
     private InputManager inputManager;
     private InventoryManager inventory;
-    private Coroutine coroutine;
-    private GameObject door;
     private RaycastHit2D hitDoor;
     #endregion
 
@@ -39,12 +44,9 @@ public class SwitchLocationDetection : MonoBehaviour
             if (inventory.isOpen) return;
         }
 
+        startPos = position;
+        startTime = time;
         hitDoor = Physics2D.Raycast(position, Vector3.forward, 20.0f, layer);
-
-        if (hitDoor)
-        {
-            coroutine = StartCoroutine(SwitchLocation(hitDoor.transform.gameObject));
-        }
     }
 
     private void EndDoor(Vector2 position, float time)
@@ -54,9 +56,18 @@ public class SwitchLocationDetection : MonoBehaviour
             if (inventory.isOpen) return;
         }
 
-        if (coroutine != null)
+        endPos = position;
+        endTime = time;
+
+        float distance = Vector3.Distance(startPos, endPos);
+        float timer = endTime - startTime;
+        if (distance <= distanceTolerance &&
+            hitDoor &&
+            timer < timerBeforeHold
+            )
         {
-            StopCoroutine(coroutine);
+            DoorScript door = hitDoor.transform.gameObject.GetComponent<DoorScript>();
+            CustomGameEvents.switchLocation.Invoke(door);
         }
     }
 
@@ -65,7 +76,5 @@ public class SwitchLocationDetection : MonoBehaviour
         Debug.Log("Change Room");
         //Animation Fade Black Screen
         yield return null;
-        DoorScript data = door.GetComponent<DoorScript>();
-        CustomGameEvents.switchLocation.Invoke(data);
     }
 }
