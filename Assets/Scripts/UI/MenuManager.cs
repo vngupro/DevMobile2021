214eda0d3c28b2c9165659934ba749f941dcb1d6
EstebanNunez <esteban78570@gmail.com>
2021-05-31 23:40:24 +0200

    add PlayService again

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -115,6 +115,6 @@ public class MenuManager : MonoBehaviour
     public void ShowAchievement()
     {
 
-        //PlayService.Instance.ShowAchievement();
+        PlayService.Instance.ShowAchievements();
     }
 }
