2ca73265a59cae5a671522358c984cfc80a38fed
EstebanNunez <esteban78570@gmail.com>
2021-06-01 16:36:04 +0200

    update GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,19 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
 
     public GameData gameData;
+    public LevelData levelData;
+    public GameObject canvasResult;
+
+    [SerializeField] private CanvasResultScript canvasResultScript;
+
+    private bool timerStop = false;
+
+    [Header ("Debug")]
+    [SerializeField] private float time;
+    [SerializeField] private float timeInCrimeScene;
+    [SerializeField] private float timeInSuspectScene;
+    
+
 
     private void Awake()
     {
@@ -19,18 +32,48 @@ public class GameManager : MonoBehaviour
             Instance = this;
         }
 
+        time = 0.0f;
 
     }
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!timerStop)
+        {
+            time += Time.deltaTime;
+        }   
+
     }
+
+    public void ExitCrimeScene()
+    {
+        timeInCrimeScene = time;
+    }
+
+    public void Accuse(bool isGuilty)
+    {
+        timeInSuspectScene = time - timeInCrimeScene;
+        timerStop = true;
+        canvasResult.SetActive(true);
+        Debug.Log("Il est gulty ? " + isGuilty);
+
+        canvasResultScript.UpdateInfo(levelData.caseTitle, levelData.totalClues.ToString(), timeInCrimeScene.ToString(), timeInSuspectScene.ToString(), isGuilty.ToString(), 2);
+    }
+
+}
+
+[System.Serializable]
+public struct LevelData
+{
+    public int level;
+    public int totalClues;
+    public string caseTitle;
+
 }
