094f1f6d841bbcd848ad90f7d44ff23a13487062
nguyen <virginie.nguyen@edu.devinci.fr>
2021-06-01 11:33:51 +0200

    debug 3

--- Assets/Scripts/Inventory/InventoryScripts/PlayerInventory.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory/Player Inventory")]
-public class PlayerInventory : ScriptableObject
-{
-    public List<InventoryItem> itemList;
-}
